7469b718c31020d2be9b80f7b36d758baa36dda9
lauraq5 <lauraquintana@colegioguanenta.edu.co>
2023-03-23 16:38:56 -0500

    se agregan los archivos

---
+++ ejercicio 1/bimestral_parte1.py
@@ -0,0 +1,15 @@
+# Bucle wile 
+
+import math 
+
+numero = int (input("Digite un numero: "))
+
+while numero<0:
+    print("Error > deberia hacer un numero positivo")
+    numero = int(input("Digite un numero: "))
+
+    print(f"/n Su raiz cuadradaes: {(math.sqrt(numero)):. 2f}")
+
+
+
+    
